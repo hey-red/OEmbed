fa4525efcc82d20698f446cda251682c86a0ee1d
red <vforviolence@outlook.com>
2022-03-19 14:41:26 +0300

    Added Imgur provider

---
+++ OEmbed.Test/ProvidersTests/ImgurTests.cs
@@ -0,0 +1,65 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class ImgurTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public ImgurTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new ImgurProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://imgur.com/gallery/KTjpf")]
+        [InlineData("https://i.imgur.com/aCRnwiC.jpeg")]
+        [InlineData("https://imgur.com/t/iu/K7e7VSI")]
+        [InlineData("https://i.stack.imgur.com/ELmEk.png")]
+        [InlineData("https://imgur.com/a/IzHjJwJ")]
+        public void UrlMatchTest(string url)
+        {
+            TestHelpers.UrlShouldMatchTest(_oEmbedProvider, url);
+        }
+
+        [Theory]
+        [InlineData("https://i.imgur.com/aCRnwiC.jpegs")]
+        [InlineData("https://s.stack.imgur.com/ELmEk.png")]
+        [InlineData("https://s.imgur.com/aCRnwiC.jpeg")]
+        [InlineData("https://imgur.com/z/IzHjJwJ")]
+        public void UrlShouldNotMatchTest(string url)
+        {
+            TestHelpers.UrlShouldNotMatchTest(_oEmbedProvider, url);
+        }
+
+        [Fact]
+        public async void RequestTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Rich>("https://i.imgur.com/aCRnwiC.jpeg");
+
+            Assert.NotNull(result);
+            Assert.Equal("rich", result!.Type);
+            Assert.Equal("1.0", result.Version);
+            Assert.Null(result.Title);
+            Assert.Null(result.AuthorName);
+            Assert.Null(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.Null(result.CacheAge);
+            Assert.Null(result.ThumbnailUrl);
+            Assert.Null(result.ThumbnailWidth);
+            Assert.Null(result.ThumbnailHeight);
+            // Rich type values
+            Assert.NotNull(result.Html);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.ProviderName);
+            _output.WriteLine(result?.ProviderUrl);
+        }
+    }
+}
---
+++ OEmbed/Providers/ImgurProvider.cs
@@ -0,0 +1,32 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    public record ImgurProvider : ProviderBase
+    {
+        public ImgurProvider()
+        {
+            AddAllowedHosts(new[]
+            {
+                "imgur.com",
+                "i.imgur.com",
+                // Partners
+                "i.stack.imgur.com"
+            });
+
+            AddScheme(
+                matcher: new RegexMatcher(@"
+                    https?://(?:(?:i\.[a-z]*\.)|i\.)?
+                    imgur\.com/
+                    (?:(?:
+                    gallery/(\w+)|
+                    t/(?:\w+)/(\w+)|
+                    a/(\w+)|
+                    (\w+)
+                    )?
+                    (?:\.[a-zA-Z]{3,4})?)"),
+                apiEndpoint: "https://api.imgur.com/oembed.json",
+                resourceType: ResourceType.Rich);
+        }
+    }
+}
