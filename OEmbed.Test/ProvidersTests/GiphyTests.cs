2e871b64ace97735239502f6e29e6fc4c3a8314d
red <vforviolence@outlook.com>
2022-03-19 08:38:03 +0300

    Added Giphy provider

---
+++ OEmbed.Test/ProvidersTests/GiphyTests.cs
@@ -0,0 +1,65 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class GiphyTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public GiphyTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new GiphyProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://giphy.com/gifs/confused-iu-looking-up-l2YWwjl8T5tdGiaf6")]
+        [InlineData("https://www.giphy.com/gifs/confused-iu-looking-up-l2YWwjl8T5tdGiaf6")]
+        [InlineData("https://media.giphy.com/media/l2YWwjl8T5tdGiaf6/giphy.gif")]
+        [InlineData("https://giphy.com/media/l2YWwjl8T5tdGiaf6/giphy.gif")]
+        [InlineData("https://giphy.com/stickers/iu-dlwlrma-uaena-PslxPYDZKuD1vNYWJy")]
+        [InlineData("https://giphy.com/clips/kpop-k-pop-red-velvet-UWq9DlocbqoIal7N7I")]
+        [InlineData("http://giphy.com/embed/l2YWwjl8T5tdGiaf6")]
+        public void GiphyMatchTest(string url)
+        {
+            Assert.True(_oEmbedProvider.CanProcess(new Uri(url)));
+        }
+
+        [Fact]
+        public async void GiphyRequestTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Photo>("https://giphy.com/gifs/confused-iu-looking-up-l2YWwjl8T5tdGiaf6");
+
+            Assert.NotNull(result);
+            Assert.Equal("photo", result!.Type);
+            Assert.Null(result.Version); // Giphy omits version
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.Null(result.CacheAge);
+            Assert.Null(result.ThumbnailUrl);
+            Assert.Null(result.ThumbnailWidth);
+            Assert.Null(result.ThumbnailHeight);
+            // Photo type values
+            Assert.NotNull(result.Url);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.AuthorName);
+            _output.WriteLine(result?.Title);
+        }
+
+        [Fact]
+        public async void GiphyDynamicResponse()
+        {
+            var result = await _oEmbedConsumer.RequestAsync("https://giphy.com/clips/kpop-k-pop-red-velvet-UWq9DlocbqoIal7N7I");
+
+            Assert.IsType<Video>(result);
+        }
+    }
+}
---
+++ OEmbed/Providers/GiphyProvider.cs
@@ -0,0 +1,29 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    public record GiphyProvider : ProviderBase
+    {
+        public GiphyProvider()
+        {
+            AddAllowedHosts(new[]
+            {
+                "giphy.com",
+                "www.giphy.com",
+                "media.giphy.com",
+            });
+
+            // Gifs, stickers, media, embed
+            AddScheme(
+                matcher: new RegexMatcher(@"https?://(?:www\.|media\.)?giphy\.com/(?:gifs|stickers|media|embed)/([\S]+)"),
+                apiEndpoint: "https://giphy.com/services/oembed",
+                resourceType: ResourceType.Photo);
+
+            // Clips
+            AddScheme(
+                matcher: new RegexMatcher(@"https?://(?:www\.)?giphy\.com/clips/([\S]+)"),
+                apiEndpoint: "https://giphy.com/services/oembed",
+                resourceType: ResourceType.Video);
+        }
+    }
+}
