b9002e8d2a05711e73be857006470e271d0e0eda
red <vforviolence@outlook.com>
2022-03-22 02:16:41 +0300

    Added Pinterest provider

---
+++ OEmbed.Test/ProvidersTests/PinterestTests.cs
@@ -0,0 +1,51 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class PinterestTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public PinterestTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new PinterestProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://www.pinterest.com/pin/6896205669260457/")]
+        public void UrlMatchTest(string url)
+        {
+            TestHelpers.UrlShouldMatchTest(_oEmbedProvider, url);
+        }
+
+        [Fact]
+        public async void RequestTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Rich>("https://www.pinterest.com/pin/6896205669260457/");
+
+            Assert.NotNull(result);
+            Assert.Equal("rich", result!.Type);
+            Assert.Equal("1.0", result.Version);
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.Null(result.CacheAge);
+            Assert.NotNull(result.ThumbnailUrl);
+            Assert.NotNull(result.ThumbnailWidth);
+            Assert.NotNull(result.ThumbnailHeight);
+            // Rich type values
+            Assert.NotNull(result.Html);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.AuthorName);
+            _output.WriteLine(result?.AuthorUrl);
+        }
+    }
+}
---
+++ OEmbed/Providers/PinterestProvider.cs
@@ -0,0 +1,20 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    /// <summary>
+    /// TODO: pin.it is unsupported by oEmbed endpoint
+    /// </summary>
+    public record PinterestProvider : ProviderBase
+    {
+        public PinterestProvider()
+        {
+            AddAllowedHosts(new[] { "www.pinterest.com", "pinterest.com" });
+
+            AddScheme(
+                matcher: new RegexMatcher(@"https?://(?:www\.)?(?:pinterest\.[a-z]+/pin/([\d]+)/?)"),
+                apiEndpoint: "https://www.pinterest.com/oembed.json",
+                resourceType: ResourceType.Rich);
+        }
+    }
+}
