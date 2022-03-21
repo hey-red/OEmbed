64c7c6ae1d6269710aae400956d15e723958ad80
red <vforviolence@outlook.com>
2022-03-22 00:20:59 +0300

    Added Pixiv provider

---
+++ OEmbed.Test/ProvidersTests/PixivTests.cs
@@ -0,0 +1,52 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class PixivTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public PixivTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new PixivProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://www.pixiv.net/en/artworks/90137533")]
+        [InlineData("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=90137533")]
+        public void UrlMatchTest(string url)
+        {
+            TestHelpers.UrlShouldMatchTest(_oEmbedProvider, url);
+        }
+
+        [Fact]
+        public async void RequestTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Rich>("https://www.pixiv.net/en/artworks/90137533");
+
+            Assert.NotNull(result);
+            Assert.Equal("rich", result!.Type);
+            Assert.Equal("1.0", result.Version);
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.Null(result.CacheAge);
+            Assert.NotNull(result.ThumbnailUrl);
+            Assert.Null(result.ThumbnailWidth);
+            Assert.Null(result.ThumbnailHeight);
+            // Rich type values
+            Assert.NotNull(result.Html);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.AuthorName);
+            _output.WriteLine(result?.Title);
+        }
+    }
+}
---
+++ OEmbed/Providers/PixivProvider.cs
@@ -0,0 +1,28 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    public record PixivProvider : ProviderBase
+    {
+        public PixivProvider()
+        {
+            AddAllowedHosts(new[] { "pixiv.net", "www.pixiv.net" });
+
+            AddScheme(
+                matcher: new RegexMatcher(
+                    /*
+                     * Old urls format
+                     * https://www.pixiv.net/member_illust.php?mode=medium&illust_id={id}
+                     */
+                    @"https?://(?:www\.)?pixiv\.net/(?:[?&=\._a-z0-9]+)illust_id=([0-9]+)",
+
+                    /*
+                     * https://www.pixiv.net/en/artworks/{id}
+                     * https://www.pixiv.net/artworks/{id}
+                     */
+                    @"https?://(?:www\.)?pixiv\.net/(?:en/)?artworks/([0-9]+)"),
+                apiEndpoint: "https://embed.pixiv.net/oembed.php",
+                resourceType: ResourceType.Rich);
+        }
+    }
+}
