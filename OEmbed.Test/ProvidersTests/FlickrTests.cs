35e49074df0e11b4f4c0d3341bf12357dcc76fb7
red <vforviolence@outlook.com>
2022-03-22 06:25:12 +0300

    Added Flickr provider

---
+++ OEmbed.Test/ProvidersTests/FlickrTests.cs
@@ -0,0 +1,82 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class FlickrTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public FlickrTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new FlickrProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://www.flickr.com/photos/191258271@N04/51414360766/in/album-72157720098386911/")]
+        [InlineData("https://flic.kr/p/2mkj3cY")]
+        [InlineData("https://www.flickr.com/photos/191258271@N04/albums/72157720098386911")]
+        [InlineData("https://www.flickr.com/photos/191258271@N04/sets/72157720098386911/")]
+        [InlineData("https://www.flickr.com/photos/191258271@N04/")]
+        [InlineData("https://www.flickr.com/photos/191258271@N04/favorites")]
+        public void UrlMatchTest(string url)
+        {
+            TestHelpers.UrlShouldMatchTest(_oEmbedProvider, url);
+        }
+
+        [Fact]
+        public async void RequestPhotoTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Photo>("https://www.flickr.com/photos/191258271@N04/51414360766/in/album-72157720098386911/");
+
+            Assert.NotNull(result);
+            Assert.Equal("photo", result!.Type);
+            Assert.NotNull(result.Version);
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.NotNull(result.CacheAge);
+            Assert.NotNull(result.ThumbnailUrl);
+            Assert.NotNull(result.ThumbnailWidth);
+            Assert.NotNull(result.ThumbnailHeight);
+            // Photo type values
+            Assert.NotNull(result.Url);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.Title);
+            _output.WriteLine(result?.Url);
+        }
+
+        [Fact]
+        public async void RequestAlbumTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Rich>("https://www.flickr.com/photos/191258271@N04/albums/72157720098386911");
+
+            Assert.NotNull(result);
+            Assert.Equal("rich", result!.Type);
+            Assert.NotNull(result.Version);
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.NotNull(result.CacheAge);
+            Assert.NotNull(result.ThumbnailUrl);
+            Assert.NotNull(result.ThumbnailWidth);
+            Assert.NotNull(result.ThumbnailHeight);
+            // Rich type values
+            Assert.NotNull(result.Html);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.Title);
+            _output.WriteLine(result?.ThumbnailUrl);
+        }
+    }
+}
---
+++ OEmbed/Providers/FlickrProvider.cs
@@ -0,0 +1,33 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    public record FlickrProvider : ProviderBase
+    {
+        public FlickrProvider()
+        {
+            AddAllowedHosts(new[]
+            {
+                "www.flickr.com",
+                "flickr.com",
+                "flic.kr"
+            });
+
+            // Photos
+            AddScheme(
+                matcher: new RegexMatcher(
+                    @"https?://(?:www\.)?flickr\.com/photos/(?:[^/]+)/([0-9]+)(?:/in/[^/\s]+)?/?",
+                    @"https?://flic\.kr/p/([a-zA-Z0-9]+)"),
+                apiEndpoint: "https://www.flickr.com/services/oembed",
+                resourceType: ResourceType.Photo);
+
+            // Albums(also known as sets), photostream, favorites
+            // NOTE: Short url for albums is not work with oembed endpoint
+            AddScheme(
+                matcher: new RegexMatcher(
+                    @"https?://(?:www\.)?flickr\.com/photos/([^/]+)(?:/(?:favorites|(?:albums|sets)/([0-9]+)))?/?"),
+                apiEndpoint: "https://www.flickr.com/services/oembed",
+                resourceType: ResourceType.Rich);
+        }
+    }
+}
