d04235f8db44701f93bc293de00c850328d1922b
red <vforviolence@outlook.com>
2022-03-23 07:54:22 +0300

    Added Vlive provider

---
+++ OEmbed.Test/ProvidersTests/VliveTests.cs
@@ -0,0 +1,51 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class VliveTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public VliveTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new VliveProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://www.vlive.tv/video/2651")]
+        public void UrlMatchTest(string url)
+        {
+            TestHelpers.UrlShouldMatchTest(_oEmbedProvider, url);
+        }
+
+        [Fact]
+        public async void RequestTest()
+        {
+            Video? result = await _oEmbedConsumer.RequestAsync<Video>("https://www.vlive.tv/video/2651");
+
+            Assert.NotNull(result);
+            Assert.Equal("video", result!.Type);
+            Assert.Equal("1.0", result.Version);
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.Null(result.CacheAge);
+            Assert.NotNull(result.ThumbnailUrl);
+            Assert.NotNull(result.ThumbnailWidth);
+            Assert.Null(result.ThumbnailHeight);
+            // Video type values
+            Assert.NotNull(result.Html);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.AuthorName);
+            _output.WriteLine(result?.Title);
+        }
+    }
+}
