3d3b41524bc58993215439c270f81e3c3691a87b
red <vforviolence@outlook.com>
2022-03-23 09:19:24 +0300

    Added Audiomack provider

---
+++ OEmbed.Test/ProvidersTests/AudiomackTests.cs
@@ -0,0 +1,54 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class AudiomackTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public AudiomackTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new AudiomackProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://audiomack.com/lilicaseley/song/pretty-good-bad-idea")]
+        [InlineData("https://audiomack.com/song/lilicaseley/pretty-good-bad-idea")]
+        [InlineData("https://audiomack.com/amber-liu/album/z")]
+        [InlineData("https://audiomack.com/michael/playlist/uk-pop")]
+        public void UrlMatchTest(string url)
+        {
+            TestHelpers.UrlShouldMatchTest(_oEmbedProvider, url);
+        }
+
+        [Fact]
+        public async void RequestTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Rich>("https://audiomack.com/lilicaseley/song/pretty-good-bad-idea");
+
+            Assert.NotNull(result);
+            Assert.Equal("rich", result!.Type);
+            Assert.Equal("1.0", result.Version);
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.Null(result.CacheAge);
+            Assert.NotNull(result.ThumbnailUrl);
+            Assert.NotNull(result.ThumbnailWidth);
+            Assert.NotNull(result.ThumbnailHeight);
+            // Rich type values
+            Assert.NotNull(result.Html);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.AuthorName);
+            _output.WriteLine(result?.Title);
+        }
+    }
+}
---
+++ OEmbed/Providers/AudiomackProvider.cs
@@ -0,0 +1,19 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    public record AudiomackProvider : ProviderBase
+    {
+        public AudiomackProvider()
+        {
+            AddAllowedHosts(new[] { "audiomack.com" });
+
+            AddScheme(
+                matcher: new RegexMatcher(
+                    @"/(?:[\w\-]+)/(?:song|album|playlist)/([\w\-]+)/?",
+                    @"/(?:song|album|playlist)/(?:[\w\-]+)/([\w\-]+)/?"),
+                apiEndpoint: "https://www.audiomack.com/oembed",
+                resourceType: ResourceType.Rich);
+        }
+    }
+}
