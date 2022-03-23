491d7489f1dce155d7ba18a6d4022c1542e0ccfb
red <vforviolence@outlook.com>
2022-03-23 09:43:52 +0300

    Added Codepen provider

---
+++ OEmbed/Providers/CodepenProvider.cs
@@ -0,0 +1,17 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    public record CodepenProvider : ProviderBase
+    {
+        public CodepenProvider()
+        {
+            AddAllowedHosts(new[] { "codepen.io" });
+
+            AddScheme(
+                matcher: new RegexMatcher(@"/(?:team/)?(?:[\w]+)/pen/([\w]+)/?"),
+                apiEndpoint: "https://codepen.io/api/oembed",
+                resourceType: ResourceType.Rich);
+        }
+    }
+}
