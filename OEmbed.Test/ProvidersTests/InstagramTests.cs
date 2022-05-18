f653fa4f18726a4d963f166de26652fc41fbde59
hey-red <vforviolence@outlook.com>
2022-05-18 17:15:16 +0300

    Remove instagram legacy endpoint

--- OEmbed.Test/ProvidersTests/InstagramTests.cs
+++ OEmbed.Test/ProvidersTests/InstagramTests.cs
@@ -6,10 +6,6 @@ namespace OEmbed.Test.ProvidersTests
     {
         private readonly ITestOutputHelper _output;
 
-        private readonly IOEmbedProvider _legacyOembedProvider;
-
-        private readonly IOEmbedConsumer _legacyOembedConsumer;
-
         private readonly IOEmbedProvider _oEmbedProvider;
 
         private readonly IOEmbedConsumer _oEmbedConsumer;
@@ -17,9 +13,6 @@ namespace OEmbed.Test.ProvidersTests
         public InstagramTests(ITestOutputHelper output)
         {
             _output = output;
-
-            _legacyOembedProvider = new InstagramProvider();
-            _legacyOembedConsumer = TestHelpers.BuildConsumer(new[] { _legacyOembedProvider });
 
             var configuration = new ConfigurationBuilder()
                 .AddUserSecrets<Secrets>()
@@ -97,42 +90,6 @@ namespace OEmbed.Test.ProvidersTests
             Assert.Null(result.Height);
 
             _output.WriteLine(result?.AuthorName);
-        }
-
-        [Theory]
-        [InlineData("https://instagram.com/p/1XSKgBAGz-")]
-        [InlineData("https://www.instagram.com/p/1XSKgBAGz-/")]
-        [InlineData("https://www.instagr.am/p/1XSKgBAGz-/")]
-        [InlineData("https://www.instagram.com/p/1XSKgBAGz-/?utm_source=ig_web_button_share_sheet")]
-        public void LegacySchemeUrlMatchTest(string url)
-        {
-            TestHelpers.UrlShouldMatchTest(_legacyOembedProvider, url);
-        }
-
-        [Fact]
-        public async void LegacySchemeRequestTest()
-        {
-            var result = await _legacyOembedConsumer.RequestAsync<Rich>("https://www.instagram.com/p/1XSKgBAGz-/");
-
-            Assert.NotNull(result);
-            Assert.Equal("rich", result!.Type);
-            Assert.Equal("1.0", result.Version);
-            Assert.NotNull(result.Title);
-            Assert.NotNull(result.AuthorName);
-            Assert.NotNull(result.AuthorUrl);
-            Assert.NotNull(result.ProviderName);
-            Assert.NotNull(result.ProviderUrl);
-            Assert.Null(result.CacheAge);
-            Assert.NotNull(result.ThumbnailUrl);
-            Assert.NotNull(result.ThumbnailWidth);
-            Assert.NotNull(result.ThumbnailHeight);
-            // Rich type values
-            Assert.NotNull(result.Html);
-            Assert.NotEqual(0, result.Width);
-            Assert.NotEqual(0, result.Height);
-
-            _output.WriteLine(result?.AuthorName);
-            _output.WriteLine(result?.Title);
         }
     }
 }
