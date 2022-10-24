b50be5830693b4a4819ade54ed90fe0b1093226e
red <vforviolence@outlook.com>
2022-10-24 09:39:03 +0300

    Remove instagram legacy endpoint tests

--- OEmbed.Test/ProvidersTests/InstagramTests.cs
+++ OEmbed.Test/ProvidersTests/InstagramTests.cs
@@ -98,41 +98,5 @@ namespace OEmbed.Test.ProvidersTests
 
             _output.WriteLine(result?.AuthorName);
         }
-
-        [Theory]
-        [InlineData("https://instagram.com/p/1XSKgBAGz-")]
-        [InlineData("https://www.instagram.com/p/1XSKgBAGz-/")]
-        [InlineData("https://www.instagr.am/p/1XSKgBAGz-/")]
-        [InlineData("https://www.instagram.com/p/1XSKgBAGz-/?utm_source=ig_web_button_share_sheet")]
-        public void LegacySchemeUrlMatchTest(string url)
-        {
-            TestHelpers.UrlShouldMatchTest(_legacyOembedProvider, url);
-        }
-
-        [Fact]
-        public async void LegacySchemeRequestTest()
-        {
-            var result = await _legacyOembedConsumer.RequestAsync<Rich>("https://www.instagram.com/p/1XSKgBAGz-/");
-
-            Assert.NotNull(result);
-            Assert.Equal("rich", result!.Type);
-            Assert.Equal("1.0", result.Version);
-            Assert.NotNull(result.Title);
-            Assert.NotNull(result.AuthorName);
-            Assert.NotNull(result.AuthorUrl);
-            Assert.NotNull(result.ProviderName);
-            Assert.NotNull(result.ProviderUrl);
-            Assert.Null(result.CacheAge);
-            Assert.NotNull(result.ThumbnailUrl);
-            Assert.NotNull(result.ThumbnailWidth);
-            Assert.NotNull(result.ThumbnailHeight);
-            // Rich type values
-            Assert.NotNull(result.Html);
-            Assert.NotEqual(0, result.Width);
-            Assert.NotEqual(0, result.Height);
-
-            _output.WriteLine(result?.AuthorName);
-            _output.WriteLine(result?.Title);
-        }
     }
 }
