d9c7e2d221ba027f9b1f0da8b849f5bd88a123b2
red <vforviolence@outlook.com>
2023-01-06 06:04:32 +0300

    Added rutube provider

---
+++ OEmbed.Test/ProvidersTests/RutubeTests.cs
@@ -0,0 +1,51 @@
+﻿namespace OEmbed.Test.ProvidersTests
+{
+    public class RutubeTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IOEmbedProvider _oEmbedProvider;
+
+        private readonly IOEmbedConsumer _oEmbedConsumer;
+
+        public RutubeTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _oEmbedProvider = new RutubeProvider();
+            _oEmbedConsumer = TestHelpers.BuildConsumer(new[] { _oEmbedProvider });
+        }
+
+        [Theory]
+        [InlineData("https://rutube.ru/video/755ec2d4b19d2374d6c78890dbaad7fe/")]
+        public void UrlMatchTest(string url)
+        {
+            TestHelpers.UrlShouldMatchTest(_oEmbedProvider, url);
+        }
+
+        [Fact]
+        public async void RequestVideoTest()
+        {
+            var result = await _oEmbedConsumer.RequestAsync<Video>("https://rutube.ru/video/755ec2d4b19d2374d6c78890dbaad7fe/");
+
+            Assert.NotNull(result);
+            Assert.Equal("video", result!.Type);
+            Assert.Equal("1", result.Version); // Spec violation
+            Assert.NotNull(result.Title);
+            Assert.NotNull(result.AuthorName);
+            Assert.NotNull(result.AuthorUrl);
+            Assert.NotNull(result.ProviderName);
+            Assert.NotNull(result.ProviderUrl);
+            Assert.NotNull(result.CacheAge);
+            Assert.NotNull(result.ThumbnailUrl);
+            Assert.NotNull(result.ThumbnailWidth);
+            Assert.NotNull(result.ThumbnailHeight);
+            // Video type values
+            Assert.NotNull(result.Html);
+            Assert.NotEqual(0, result.Width);
+            Assert.NotEqual(0, result.Height);
+
+            _output.WriteLine(result?.AuthorName);
+            _output.WriteLine(result?.Title);
+        }
+    }
+}
---
+++ OEmbed/Providers/RutubeProvider.cs
@@ -0,0 +1,20 @@
+﻿using HeyRed.OEmbed.Providers.Common;
+
+namespace HeyRed.OEmbed.Providers
+{
+    public record RutubeProvider : ProviderBase
+    {
+        public RutubeProvider()
+        {
+            AddAllowedHosts(new[]
+            {
+                "rutube.ru",
+            });
+
+            AddScheme(
+                matcher: new RegexMatcher("/video/([a-zA-Z0-9]+)/"),
+                apiEndpoint: "https://rutube.ru/api/oembed/",
+                resourceType: ResourceType.Video);
+        }
+    }
+}
